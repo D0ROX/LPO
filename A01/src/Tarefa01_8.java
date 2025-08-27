0af593b4e39e0646f309adeadecb4e07a2cc71d2
Danniel Eduardo Dorox <ddorox@curitiba.prefs>
2025-08-27 10:57:16 -0300

    Primeiro commit

---
+++ A01/src/Tarefa01_8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tarefa01_8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String nome;
+
+        System.out.println("Informe seu primeiro e seu ultimo nome: ");
+        nome = sc.nextLine();
+
+        String[] separaNome = nome.split(" ");
+
+        for(int i = separaNome.length-1; i >= 0; i--){
+            System.out.println(separaNome[i]);
+        }
+    }
+}
